e87c3660c0fdaee98f954a7ff6ff301d61147a1c
Pedro Araujo <pedroaraujo.cdc@gmail.com>
2026-03-16 18:32:15 +0000

    Inicando Erros e Excecoes

--- src/br/com/pedroaraujocdc/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/pedroaraujocdc/screenmatch/principal/PrincipalComBusca.java
@@ -37,9 +37,18 @@ public class PrincipalComBusca {
 
         TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
         System.out.println(meuTituloOmdb);
-        Titulo meuTitulo = new Titulo(meuTituloOmdb);
-        System.out.println("Titulo ja convertido");
-        System.out.println(meuTitulo);
+        try {
+            Titulo meuTitulo = new Titulo(meuTituloOmdb);
+            System.out.println("Titulo ja convertido");
+            System.out.println(meuTitulo);
+        } catch (NumberFormatException e){
+            System.out.println("Aconteceu um erro: ");
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("O programa finalizou corretamente!");
+
+
 
 
 
