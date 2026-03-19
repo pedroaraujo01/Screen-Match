2cce85059929397e53036230ea4debbf349d26ef
Pedro Araujo <pedroaraujo.cdc@gmail.com>
2026-03-19 13:10:13 +0000

    Utilizando FileWriter

--- src/br/com/pedroaraujocdc/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/pedroaraujocdc/screenmatch/principal/PrincipalComBusca.java
@@ -7,6 +7,7 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -42,6 +43,10 @@ public class PrincipalComBusca {
             Titulo meuTitulo = new Titulo(meuTituloOmdb);
             System.out.println("Titulo ja convertido");
             System.out.println(meuTitulo);
+
+            FileWriter escrita = new FileWriter("Filmes.txt");
+            escrita.write((meuTitulo.toString()));
+            escrita.close();
         } catch (NumberFormatException e){
             System.out.println("Aconteceu um erro: ");
             System.out.println(e.getMessage());
